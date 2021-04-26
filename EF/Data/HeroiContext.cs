9098eda10dbaeb8565917526fe8b6f9f79a4a26c
Adalberto Rolim Nogueira <adalberto.nogueira@autotrac.com.br>
2021-04-26 18:14:34 -0300

    Inicia implementação

---
+++ EF/Data/HeroiContext.cs
@@ -0,0 +1,16 @@
+﻿using EF.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+    }
+}
---
+++ EF/Model/Heroi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF.Model
+{
+    public class Heroi
+    {
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public Batalha Batalha { get; set; }
+        public int BatalhaId { get; set; }
+    }
+}
